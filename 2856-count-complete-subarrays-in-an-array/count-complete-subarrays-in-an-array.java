4c0a054bb0a3da7b37fe93d453702c328893a820
jasvindersingh2-0 <130572749+jasvindersingh2-0@users.noreply.github.com>
2025-04-24 12:15:14 +0530

    Time: 7 ms (89.07%) | Memory: 45.2 MB (47.27%) - LeetSync

--- 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.java
+++ 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.java
@@ -1,4 +1,5 @@
 class Solution {
+    // Approah -> agar ek sub array ke distinct elements total distict element ke equal hai matlab jis point par apan khade hai uske aage ke jitne bhi sub arrays aayenge unke distinct elements bhi total ke equal hi honge(ex - agar ek array mein 3 distinct element hai or ek subarray mein bhi utne hi distinct ele hai to aage mtlab koi naya element to ho nahi sakta to yahi se aage ke sab array ko le lo) to unn sab lo issi point par jode liya
     public int countCompleteSubarrays(int[] nums) {
         int n = nums.length;
         HashSet<Integer>st = new HashSet();
@@ -6,15 +7,14 @@ class Solution {
             st.add(i);
         }
         int total = st.size();
-        // st.clear();
         HashMap<Integer,Integer>mp = new HashMap<>();
         int ptr1 = 0;
         int ptr2 = 0;
         int ans = 0;
         while(ptr2<n){
             mp.put(nums[ptr2],mp.getOrDefault(nums[ptr2],0)+1);
-            while(mp.size()==total){
-                ans += n - ptr2;
+            while(ptr1<=ptr2 && mp.size()==total){
+                ans += n - ptr2; // adding all subarrays including this to end of array
                 mp.put(nums[ptr1],mp.get(nums[ptr1])-1);
                 if(mp.get(nums[ptr1]) == 0)mp.remove(nums[ptr1]);
                 ptr1++;
