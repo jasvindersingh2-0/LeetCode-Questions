b3040fb9ec2b43f885ef09aff18395e575f95d14
jasvindersingh2-0 <130572749+jasvindersingh2-0@users.noreply.github.com>
2025-05-08 13:43:09 +0530

    Time: 9 ms (42.99%) | Memory: 44.8 MB (76.12%) - LeetSync

--- 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.java
+++ 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.java
@@ -1,43 +1,27 @@
 class Solution {
-   static public int minTimeToReach(int[][] moveTime) {
-        int r = moveTime.length, c = moveTime[0].length;
-        int[][] minimumTime = new int[r][c];
-        for (int[] is : minimumTime) {
-            Arrays.fill(is, Integer.MAX_VALUE);
+    public int minTimeToReach(int[][] moveTime) {
+        int m = moveTime.length;
+        int n = moveTime[0].length;
+        int[][]dis = new int[m][n];
+        for(int[]i : dis)Arrays.fill(i,Integer.MAX_VALUE);
+        PriorityQueue<int[]>pq = new PriorityQueue<>((a,b)->a[2]-b[2]);
+        pq.offer(new int[]{0,0,0});
+        while(!pq.isEmpty()){
+            int[]curr = pq.poll();
+            int u = curr[0];
+            int v = curr[1];
+            int w = curr[2];
+            for(int[]i : new int[][]{{0,1},{1,0},{0,-1},{-1,0}}){
+                int r = u + i[0];
+                int c = v + i[1];
+                if(r<0 || c<0 || r>=m || c>=n)continue;
+                int cost = Math.max(moveTime[r][c],dis[u][v]+1);
+                if(dis[r][c]>cost){
+                    dis[r][c] = cost;
+                    pq.offer(new int[]{r,c,cost});
+                }
+            }
         }
-
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> a.steps - b.steps);
-        pq.add(new Pair(-1, 0, 0));
-        minimumTime[0][0] = 0;
-
-        while (!pq.isEmpty()) {
-            Pair top = pq.poll();
-            int i = top.i, j = top.j, nextStep = top.steps + 1;
-            if (i + 1 < r) update(i + 1, j, pq, nextStep, moveTime, minimumTime);
-            if (i - 1 >= 0) update(i - 1, j, pq, nextStep, moveTime, minimumTime);
-            if (j - 1 >= 0) update(i, j - 1, pq, nextStep, moveTime, minimumTime);
-            if (j + 1 < c)  update(i, j + 1, pq, nextStep, moveTime, minimumTime);
-            if(minimumTime[r-1][c-1] != Integer.MAX_VALUE) return minimumTime[r-1][c-1] + 1;
-        }
-        return minimumTime[r-1][c-1] + 1;
-    }
-
-    static void update(int i, int j, PriorityQueue<Pair> pq, int nextStep, int[][] moveTime, int[][] minimumTime){
-        if (nextStep < moveTime[i][j]) nextStep = moveTime[i][j];
-        if (minimumTime[i][j] > nextStep) {
-            pq.add(new Pair(nextStep, i, j));
-            minimumTime[i][j] = nextStep;
-        }
-    }
-
-}
-
-class Pair {
-    int steps = 0, i = -1, j = -1;
-
-    public Pair(int steps, int i, int j) {
-        this.steps = steps;
-        this.i = i;
-        this.j = j;
+        return dis[m-1][n-1]+1;
     }
 }
