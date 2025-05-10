770fd8afc61d1ffb76fac7983d8624cad299b11f
jasvindersingh2-0 <130572749+jasvindersingh2-0@users.noreply.github.com>
2025-05-10 14:26:00 +0530

    Time: 3 ms (88.73%) | Memory: 60.8 MB (47.42%) - LeetSync

--- 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/minimum-equal-sum-of-two-arrays-after-replacing-zeros.java
+++ 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/minimum-equal-sum-of-two-arrays-after-replacing-zeros.java
@@ -1,56 +1,28 @@
 class Solution {
     // Tip ->It's actually really simple. Iterate over both arrays and get their sums and number of zeros. Every time you see a zero, add 1 to your sum count because that is the minimum possible value they could be. Next you will have num1's minimum possible sum and num2's minimum possible sum. We never want to make the larger of those two sums any greater. How can we determine whether or not the smaller sum can catch up?
     public long minSum(int[] nums1, int[] nums2) {
-        long sum1 = 0;
-        long sum2 = 0;
         long zero1 = 0;
         long zero2 = 0;
+        long sum1 = 0;
+        long sum2 = 0;
         for(int i : nums1){
-            if(i==0)zero1++;
             sum1+=i;
+            if(i == 0){
+                zero1++;
+                sum1++;
+            }
         }
         for(int i : nums2){
-            if(i==0)zero2++;
             sum2+=i;
+            if(i ==0){
+                sum2++;
+                zero2++;
+            }
         }
-        // System.out.println("sum1 "+sum1+" "+"zero1 "+zero1);
-        // System.out.println("sum2 "+sum2+" "+"zero2 "+zero2);
-        if(sum1 == sum2){
-            if(zero1 == 0 && zero2 == 0)return sum1;
-            else if(zero1 == 0 && zero1 != 0)return -1;
-            else if(zero1 != 0 && zero2 == 0)return -1;
-            else return sum1 + Math.max(zero1 , zero2);
-        }
-        if(sum1 != sum2 &&(zero1 == 0 && zero2 ==0))return -1;
-        if(sum1 == sum2 && (zero1 != 0 && zero2 != 0))return sum1 + Math.max(zero1,zero2);
-        
-        // if(zero1 == 0 || zero2 == 0)return -1;
-        int max = 0;
-        if(sum1>sum2 && zero1>=zero2){
-            if(zero2 == 0)return -1;
-            // if(zero2>sum1-sum2)return -1;
-            return sum1+zero1;
-        }
-        else if(sum1>sum2 && zero1<zero2){
-            if(sum1<sum2+zero2 && zero1 == 0)return -1;
-            if(sum1+zero1>=sum2+zero2)return sum1+zero1;
-            else return sum2+zero2;
-        }
-        else if(sum2>sum1 && zero2>=zero1){
-            if(zero1==0)return -1;
-            return sum2+zero2;
-        }
-        else if(sum2>sum1 && zero1>zero2){
-            if(sum2<sum1+zero1 && zero2 == 0)return -1;
-            if(sum2+zero2>sum1+zero1)return sum2+zero2;
-            else return sum1+zero1;
-        }
-        
-        else{
-            if(zero1 == 0)return -1;
-            if(zero1>sum2-sum1)return -1;
-            return sum2+zero2;
-        }
-        // return 0;
+        // System.out.println("Sum1 "+sum1+" Zero1 "+zero1);
+        // System.out.println("Sum2 "+sum2+" Zero2 "+zero2);
+        if(sum1<sum2 && zero1 == 0)return -1;
+        if(sum2<sum1 && zero2 == 0)return -1;
+        return Math.max(sum1,sum2);
     }
 }
