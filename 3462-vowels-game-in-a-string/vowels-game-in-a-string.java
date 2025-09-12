cada4ab90c3fb0ce1f996947664aac6edfcb9ba4
jasvindersingh2-0 <130572749+jasvindersingh2-0@users.noreply.github.com>
2025-09-12 22:50:55 +0530

    Time: 4 ms (79.79%) | Memory: 45.4 MB (92.65%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.java
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.java
@@ -1,12 +1,10 @@
 class Solution {
     public boolean doesAliceWin(String s) {
-        int count = 0;
         for(char c : s.toCharArray()){
             if(c=='a'||c =='e'||c=='i'||c=='o'||c =='u'){
-                count++;
+                return true;
             }
         }
-        if(count == 0)return false;
-        return true;
+        return false;
     }
 }
