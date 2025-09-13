eae0028332e24e2010357c7fc8547c34991b329a
jasvindersingh2-0 <130572749+jasvindersingh2-0@users.noreply.github.com>
2025-09-13 15:46:56 +0530

    Time: 2 ms (66.22%) | Memory: 42.2 MB (91.95%) - LeetSync

---
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxFreqSum(String s) {
+        int maxCon = 0;
+        int maxVov = 0;
+        int[]arr = new int[26];
+        for(char c : s.toCharArray()){
+            if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'){
+                arr[c-'a']++;
+                if(arr[c-'a']>maxVov)maxVov = arr[c-'a'];
+            }
+            else{
+                arr[c-'a']++;
+                if(arr[c-'a']>maxCon)maxCon = arr[c-'a'];
+            }
+        }
+        return maxVov + maxCon;
+    }
+}
