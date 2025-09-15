c47d7770e5763b25756821695798044212018e80
jasvindersingh2-0 <130572749+jasvindersingh2-0@users.noreply.github.com>
2025-09-15 23:26:14 +0530

    Time: 4 ms (27.38%) | Memory: 42.9 MB (29.70%) - LeetSync

---
+++ 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int canBeTypedWords(String text, String brokenLetters) {
+        HashSet<Character>st = new HashSet();
+        for(char c : brokenLetters.toCharArray()){
+            st.add(c);
+        }
+        String[]arr = text.split(" ");
+        int ans = 0;
+        for(String s : arr){
+            boolean flag = true;
+            for(char c : s.toCharArray()){
+                if(st.contains(c)){
+                    flag = false;
+                }
+            }
+            if(flag)ans++;
+        }
+        return ans;
+    }
+}
